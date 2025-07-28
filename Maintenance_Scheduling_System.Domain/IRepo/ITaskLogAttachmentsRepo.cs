50d053fdeddf9b5b73b9197d56a7ecef40b833eb
Faizan Shahid <faizan.gamer68@gmail.com>
2025-07-28 13:01:41 +0500

    Added Missing Repository Interface and Implementation

---
+++ Maintenance_Scheduling_System.Domain/IRepo/ITaskLogAttachmentsRepo.cs
@@ -0,0 +1,17 @@
+﻿using Maintenance_Scheduling_System.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maintenance_Scheduling_System.Domain.IRepo
+{
+    public interface ITaskLogAttachmentsRepo
+    {
+        public Task CreateNewTaskLogAttachment(TaskLogAttachment tasklogs);
+        public Task DeleteTaskLogAttachment(TaskLogAttachment taskLogs);
+        public Task UpdateTaskLogAttachment(TaskLogAttachment tasklogs);
+        public Task<List<TaskLogAttachment>> GetAllTaskLogAttachmentByTaskLogId(int LogId);
+    }
+}
---
+++ Maintenance_Scheduling_System.Infrastructure/Repositories/TaskLogAttachmentRepository.cs
@@ -0,0 +1,51 @@
+﻿using Maintenance_Scheduling_System.Domain.Entities;
+using Maintenance_Scheduling_System.Domain.IRepo;
+using Maintenance_Scheduling_System.Infrastructure.DbContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Maintenance_Scheduling_System.Infrastructure.Repositories
+{
+    public class TaskLogAttachmentRepository : ITaskLogAttachmentsRepo
+    {
+        public Maintenance_DbContext DbContext { get; set; }
+
+        public TaskLogAttachmentRepository(Maintenance_DbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public async Task CreateNewTaskLogAttachment(TaskLogAttachment tasklogs)
+        {
+            await DbContext.TaskLogsAttachments.AddAsync(tasklogs);
+            await DbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteTaskLogAttachment(TaskLogAttachment taskLogs)
+        {
+            taskLogs.IsDeleted = true;
+            taskLogs.LastModifiedAt = DateTime.UtcNow;
+
+            DbContext.TaskLogsAttachments.Update(taskLogs);
+            await DbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<TaskLogAttachment>> GetAllTaskLogAttachmentByTaskLogId(int logId)
+        {
+            return await DbContext.TaskLogsAttachments
+                .Where(t => !t.IsDeleted && t.LogId == logId)
+                .ToListAsync();
+        }
+
+        public async Task UpdateTaskLogAttachment(TaskLogAttachment tasklogs)
+        {
+            tasklogs.LastModifiedAt = DateTime.UtcNow;
+
+            DbContext.TaskLogsAttachments.Update(tasklogs);
+            await DbContext.SaveChangesAsync();
+        }
+    }
+}
