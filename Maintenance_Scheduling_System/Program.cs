71a8d054871585be1cfd6ae771aa4c6dae92bfff
Faizan Shahid <faizan.gamer68@gmail.com>
2025-07-29 10:47:45 +0500

    Added the required services to run the application

--- Maintenance_Scheduling_System/Program.cs
+++ Maintenance_Scheduling_System/Program.cs
@@ -1,8 +1,14 @@
 
+using Maintenance_Scheduling_System.Application.Interfaces;
+using Maintenance_Scheduling_System.Application.Services;
+using Maintenance_Scheduling_System.Application.Setting;
 using Maintenance_Scheduling_System.Domain.Entities;
+using Maintenance_Scheduling_System.Domain.IRepo;
 using Maintenance_Scheduling_System.Infrastructure.DbContext;
+using Maintenance_Scheduling_System.Infrastructure.Repositories;
 using Maintenance_Scheduling_System.Infrastructure.Settings;
 using Microsoft.AspNetCore.Identity;
+using System.Reflection;
 
 namespace Maintenance_Scheduling_System
 {
@@ -21,15 +27,35 @@ namespace Maintenance_Scheduling_System
 
             builder.Services.Configure<ConnectionSettings>(builder.Configuration.GetSection("ConnectionStrings"));
 
+            builder.Services.AddAutoMapper(typeof(AssemblyReference).Assembly);
+
+            builder.Services.AddHttpContextAccessor();
+
             builder.Services.AddDbContext<Maintenance_DbContext>();
 
             builder.Services.AddIdentity<AppUser, IdentityRole>()
                 .AddEntityFrameworkStores<Maintenance_DbContext>()
                 .AddDefaultTokenProviders();
 
-            builder.Services.AddScoped<WeatherForecast>();
+            builder.Services.AddScoped<ICurrentUser, FakeCurrentUser>();
+
+            builder.Services.AddScoped<IEquipmentRepo, EquipmentRepository>();
+            builder.Services.AddScoped<IMaintenanceHistoryRepo, MaintenanceHistoryRepository>();
+            builder.Services.AddScoped<IMainTaskRepo, MainTaskRepository>();
+
+            builder.Services.AddScoped<IEquipmentService,EquipmentService>();
+            builder.Services.AddScoped<IMaintenanceHistoryService, MaintenanceHistoryService>();
+            builder.Services.AddScoped<IMainTaskService,MainTaskService>();
 
             var app = builder.Build();
+
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                SeedData.Seed(services);
+            }
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
