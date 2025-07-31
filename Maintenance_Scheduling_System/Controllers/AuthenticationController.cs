be128d01e7e728b645028d6b66d02ab6803c54ef
Faizan Shahid <faizan.gamer68@gmail.com>
2025-07-31 15:26:17 +0500

    Implemented these 2

---
+++ Maintenance_Scheduling_System/Controllers/AuthenticationController.cs
@@ -0,0 +1,81 @@
+﻿using Maintenance_Scheduling_System.Application.DTO.AppUserDTOs;
+using Maintenance_Scheduling_System.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Maintenance_Scheduling_System.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAppUserService _appUserService;
+
+        public AuthenticationController(IAppUserService appUserService)
+        {
+            _appUserService = appUserService;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CreateTechnician([FromBody] AppUserDTO request)
+        {
+            await _appUserService.CreateAppUser(request, "Technician");
+            return Ok("Technician created successfully.");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> CreateAdmin([FromQuery] AppUserDTO request)
+        {
+            await _appUserService.CreateAppUser(request, "Admin");
+            return Ok("Admin created successfully.");
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteTechnician(string id)
+        {
+            await _appUserService.DeleteAppUser(id);
+            return Ok("User soft-deleted successfully.");
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> UpdateTechnician(string id, [FromBody] AppUserDTO dto)
+        {
+            await _appUserService.UpdateAppUser(id, dto);
+            return Ok("User updated successfully.");
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetTechnicians()
+        {
+               var technicians = await _appUserService.GetAllTechnicianUsers();
+               return Ok(technicians);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromQuery] LoginDTO login)
+        {
+            var user = await _appUserService.Login(login);
+
+            if (user == null)
+                return Unauthorized("Invalid credentials");
+
+            var token = await _appUserService.CreateToken(user);
+            return Ok(token);
+        }
+
+        [Route("api/test-authorize")]
+        [HttpGet]
+        
+        public IActionResult TestAuth()
+        {
+            return Ok("You are authorized!");
+        }
+
+    }
+}
---
+++ Maintenance_Scheduling_System/Controllers/MaintenanceScheduleController.cs
@@ -0,0 +1,104 @@
+﻿using Maintenance_Scheduling_System.Application.DTO.MaintenanceScheduleDTOs;
+using Maintenance_Scheduling_System.Application.DTO.ScheduleTaskDTOs;
+using Maintenance_Scheduling_System.Application.Interfaces;
+using Maintenance_Scheduling_System.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Maintenance_Scheduling_System.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[Action]")]
+    public class MaintenanceScheduleController : ControllerBase
+    {
+        private IMaintenanceScheduleService MaintenanceScheduleService {  get; set; }
+
+        public MaintenanceScheduleController(IMaintenanceScheduleService maintenanceScheduleService)
+        {
+            MaintenanceScheduleService = maintenanceScheduleService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMaintenanceSchedule([FromBody] CreateMaintenanceScheduleDTO dto)
+        {
+            await MaintenanceScheduleService.CreateMaintenanceSchedule(dto);
+            return Ok("Schedule created.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateMaintenanceSchedule([FromBody] MaintenanceScheduleDTO dto)
+        {
+            await MaintenanceScheduleService.UpdateMaintenanceSchedule(dto);
+            return Ok("Schedule updated.");
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteMaintenanceSchedule(int id)
+        {
+            await MaintenanceScheduleService.DeleteMaintenanceSchedule(id);
+            return Ok("Schedule deleted.");
+        }
+
+        [HttpPost("{scheduleId:int}/task")]
+        public async Task<IActionResult> AddNewTaskToSchedule(int scheduleId, [FromBody] CreateScheduleTaskDTO dto)
+        {
+            await MaintenanceScheduleService.AddNewTasktoSchedule(scheduleId, dto);
+            return Ok("Task added to schedule.");
+        }
+
+        [HttpDelete("{scheduleId:int}/task/{taskId:int}")]
+        public async Task<IActionResult> DeleteTaskFromSchedule(int scheduleId, int taskId)
+        {
+            await MaintenanceScheduleService.DeleteTaskFromSchedule(scheduleId, taskId);
+            return Ok("Task removed from schedule.");
+        }
+
+        [HttpGet("{equipmentId:int}")]
+        public async Task<IActionResult> GetScheduleByEquipment(int equipmentId)
+        {
+            var result = await MaintenanceScheduleService.GetMaintenanceScheduleByEquipmentId(equipmentId);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMaintenanceSchedules()
+        {
+            var result = await MaintenanceScheduleService.GetAllMaintenanceSchedule();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllSortedByStartDate()
+        {
+            var result = await MaintenanceScheduleService.GetAllMaintenanceScheduleByStartDate();
+            return Ok(result);
+        }
+
+        [HttpPut("{scheduleId}")]
+        public async Task<IActionResult> ActivateSchedule(int scheduleId)
+        {
+            try
+            {
+                await MaintenanceScheduleService.ActivateSchedule(scheduleId);
+                return Ok(new { message = "Schedule activated successfully." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+
+        [HttpPut("{scheduleId}")]
+        public async Task<IActionResult> UnActivateSchedule(int scheduleId)
+        {
+            try
+            {
+                await MaintenanceScheduleService.UnActivateSchedule(scheduleId);
+                return Ok(new { message = "Schedule deactivated successfully." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+    }
+}
