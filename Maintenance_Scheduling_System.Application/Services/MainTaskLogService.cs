a4bb978d5378b4e4cd60b6c2ec40bccfed8e33f9
Faizan Shahid <faizan.gamer68@gmail.com>
2025-08-29 17:47:37 +0500

    Added and Implemented the following Services

---
+++ Maintenance_Scheduling_System.Application/Services/MainTaskLogService.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Maintenance_Scheduling_System.Application.DTO.MainTaskDTOs;
+using Maintenance_Scheduling_System.Application.DTO.TaskLogDTOs;
+using Maintenance_Scheduling_System.Application.Interfaces;
+using Maintenance_Scheduling_System.Domain.Entities;
+using Maintenance_Scheduling_System.Domain.IRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maintenance_Scheduling_System.Application.Services
+{
+    public class MainTaskLogService : IMainTaskLogService
+    {
+        public IMapper mapper { get; set; }
+        public IMainTaskRepo MainTaskRepository { get; set; }
+        public ITaskLogRepo TaskLogRepository { get; set; }
+
+        public ITaskLogAttachmentService TaskLogAttachmentService { get; set; }
+
+        public ICurrentUser currentUser { get; set; }
+
+        public MainTaskLogService(IMapper map,IMainTaskRepo Mainrepo, ITaskLogRepo logrepo, ICurrentUser user, ITaskLogAttachmentService taskLogAttachmentService)
+        {
+            mapper = map;
+            MainTaskRepository = Mainrepo;
+            TaskLogRepository = logrepo;
+            currentUser = user;
+            TaskLogAttachmentService = taskLogAttachmentService;
+        }
+        private void AuditModify(TaskLogs equip)
+        {
+            equip.LastModifiedAt = DateTime.Now;
+            equip.LastModifiedBy = currentUser.Name;
+        }
+        public async Task CreateTaskLog(CreateTaskLogDTO taskdto)
+        {
+            var Mtask = await MainTaskRepository.GetTaskById(taskdto.TaskId);
+            var tasklog = mapper.Map<TaskLogs>(taskdto);
+
+            tasklog.TaskId = taskdto.TaskId;
+            tasklog.CreatedBy = currentUser.Name;
+
+            AuditModify(tasklog);
+
+            await TaskLogRepository.CreateNewTaskLogs(tasklog);
+        }
+
+        public async Task<List<TaskLogDTO>> GetAllTaskLog(int TaskId)
+        {
+            var logs = await TaskLogRepository.GetAllTaskLogsByTaskId(TaskId);
+            var logdto = mapper.Map<List<TaskLogDTO>>(logs);
+            return logdto;
+        }
+
+        public async Task UpdateTaskLog(TaskLogDTO tasklog)
+        {
+            var log = await TaskLogRepository.GetTaskLogByLogId(tasklog.LogId);
+            log.Note = tasklog.Note;
+            log.LCreatedAt = tasklog.CreatedAt;
+            log.LCreatedBy = tasklog.CreatedBy;
+            AuditModify(log);
+
+            await TaskLogRepository.UpdateTaskLogs(log);
+        }
+
+        public async Task DeleteTaskLog(int logId)
+        {
+            var log = await TaskLogRepository.GetTaskLogByLogId(logId);
+            log.IsDeleted = true;
+            AuditModify(log);
+
+            await TaskLogRepository.DeleteTaskLogs(log);
+
+            await TaskLogAttachmentService.DeleteAttachmentByLog(logId);
+        }
+    }
+}
---
+++ Maintenance_Scheduling_System.Application/Services/TaskLogAttachmentService.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using Maintenance_Scheduling_System.Application.Interfaces;
+using Maintenance_Scheduling_System.Domain.Entities;
+using Maintenance_Scheduling_System.Domain.IRepo;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maintenance_Scheduling_System.Application.Services
+{
+    public class TaskLogAttachmentService : ITaskLogAttachmentService
+    {
+   
+        public ITaskLogRepo TaskLogRepository { get; set; }
+        public ITaskLogAttachmentsRepo LogAttachmentRepository { get; set; }
+        public ICurrentUser currentUser { get; set; }
+
+        public TaskLogAttachmentService(ITaskLogRepo logrepo, ITaskLogAttachmentsRepo attachrepo, ICurrentUser user)
+        {
+            TaskLogRepository = logrepo;
+            LogAttachmentRepository = attachrepo;
+            currentUser = user;
+        }
+
+        public async Task<TaskLogAttachment> DownloadAttachment(int attachId)
+        {
+            var attachment = await LogAttachmentRepository.GetTaskLogAttachmentById(attachId);
+
+            if (attachment == null || attachment.IsDeleted)
+                throw new FileNotFoundException("Attachment not found.");
+
+            return attachment;
+        }
+
+        public async Task DeleteAttachment(int attachId)
+        {
+            var attach = await LogAttachmentRepository.GetTaskLogAttachmentById(attachId);
+
+            attach.IsDeleted = true;
+            attach.LastModifiedAt = DateTime.Now;
+            attach.LastModifiedBy = currentUser.Name;
+
+            await LogAttachmentRepository.DeleteTaskLogAttachment();
+
+        }
+        public async Task DeleteAttachmentByLog(int Log)
+        {
+            var attach1 = await LogAttachmentRepository.GetAllTaskLogAttachmentByTaskLogId(Log);
+
+            foreach (var attach in attach1)
+            {
+                attach.IsDeleted = true;
+                attach.LastModifiedAt = DateTime.Now;
+                attach.LastModifiedBy = currentUser.Name;
+            }
+
+            await LogAttachmentRepository.DeleteTaskLogAttachment();
+
+        }
+
+        public async Task UploadAttachment(int logId, IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                throw new ArgumentException("Invalid file.");
+
+            var log = await TaskLogRepository.GetTaskLogByLogId(logId);
+
+            if (log == null)
+                throw new Exception("Task log not found.");
+
+            string uploadsFolder = "C:\\Users\\Administrator\\source\\repos\\Maintenance_Scheduling_System\\Maintenance_Scheduling_System\\Uploads";
+
+            if (!Directory.Exists(uploadsFolder))
+                Directory.CreateDirectory(uploadsFolder);
+
+            string uniqueFileName = $"{Guid.NewGuid()}_{file.FileName}";
+
+            string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            var attachment = new TaskLogAttachment
+            {
+                FileName = file.FileName,
+                FilePath = $"Uploads/{uniqueFileName}",
+                ContentType = file.ContentType,
+                LogId = logId,
+                CreatedAt = DateTime.Now,
+                CreatedBy = currentUser.Name,
+                LastModifiedAt = DateTime.Now,
+                LastModifiedBy = currentUser.Name
+            };
+
+            await LogAttachmentRepository.CreateNewTaskLogAttachment(attachment);
+        }
+
+    }
+}
